03592e4df243d45e1fabb6cb7ad0183a1b5c168b
Hashim <104575878+abdallah1hashim@users.noreply.github.com>
2023-11-24 23:18:54 +0200

    ..

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 let btn = document.querySelectorAll(".btn");
+let form = document.forms[0];
+
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function () {
     if (btn[i].classList.contains("like")) {
@@ -14,3 +16,25 @@ for (let i = 0; i < btn.length; i++) {
     }
   });
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!$("input[name=name]").val()) {
+    alert("Name field is required.");
+    return;
+  }
+  if (!$("input[name=email]").val()) {
+    alert("Email field is required.");
+    return;
+  }
+
+  if (!$("input[name=phone]").val()) {
+    alert("Phone field is required.");
+    return;
+  }
+  if (!$("textarea[name=message]").val()) {
+    alert("Message field is required.");
+    return;
+  }
+  $("#success_message").show();
+});
